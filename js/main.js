35875266e904fcb8b87a74b509ec2b8f8118a7d7
Haczi <dawidhaczela@gmail.com>
2022-03-19 00:38:09 +0000

    finished web

--- js/main.js
+++ js/main.js
@@ -1,18 +1,25 @@
 const navMobile = document.querySelector('.nav-mobile');
 const navBtn = document.querySelector('.hamburger');
 const footerYear = document.querySelector('.footer__year');
-
+const allNavItems = document.querySelectorAll('nav')
 
 
 const openNav = () => {
     navBtn.classList.toggle('is-active');
     navMobile.classList.toggle('nav-mobile--active')
+
+    allNavItems.forEach(item => {
+        item.addEventListener('click', () => {
+        navMobile.classList.remove ('nav-mobile--active')
+        })
+    })
+
 }
 
 
 
 
-navBtn.addEventListener('click', openNav)
+navBtn.addEventListener('click', openNav);
 
 
 
